e00fee91c5fd83816d3f17997604c72d1c95df7a
chaoyuepan <chaoyuepan@baidu.com>
2021-06-14 11:39:27 +0800

    add example

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/panicthis/main
+
+go 1.16
+
+require (
+	github.com/panicthis/A v1.2.0
+	github.com/panicthis/B v1.2.0
+)
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/panicthis/A"
+	"github.com/panicthis/B"
+)
+
+func main() {
+	A.Visit()
+	B.Visit()
+}
